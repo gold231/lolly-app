f19ba2305520238e2c053ad9db12ae2c572872fe
gold231 <goldendragon0318@gmail.com>
2021-07-28 01:49:31 -0400

    change background

--- src/pages/Videopage.js
+++ src/pages/Videopage.js
@@ -1,12 +1,12 @@
-import { useState, useEffect, useRef } from "react"
+import { useState, useEffect, useRef } from "react";
 import { useParams } from "react-router-dom";
 
-import Appstore from "../assets/img/appstore.png"
-import Heart from "../assets/img/heart.png"
-import Play from "../assets/img/play.png"
-import "../assets/styles/_videopage.scss"
+import Appstore from "../assets/img/appstore.png";
+import Heart from "../assets/img/heart.png";
+import Play from "../assets/img/play.png";
+import "../assets/styles/_videopage.scss";
 
-import axios from "axios"
+import axios from "axios";
 
 const detail = {
   firstName: "",
@@ -16,16 +16,16 @@ const detail = {
   bio: "",
   cloudflareId: "",
   videoWidth: "",
-  videoHeight: ""
-}
+  videoHeight: "",
+};
 
 function Videopage() {
   let { id } = useParams();
-  const [data, setData] = useState(detail)
-  const [videoURL, setVideoURL] = useState("")
-  const [age, setAge] = useState("")
+  const [data, setData] = useState(detail);
+  const [videoURL, setVideoURL] = useState("");
+  const [age, setAge] = useState("");
 
-  const [flag, setFlag] = useState(0)
+  const [flag, setFlag] = useState(0);
   const [playFlag, setPlayFlag] = useState(false);
   const [playFlag2, setPlayFlag2] = useState(false);
 
@@ -36,15 +36,14 @@ function Videopage() {
     let headers = {
       "content-type": "application/json",
       "Access-Control-Allow-Credentials": "true",
-      "Access-Control-Allow-Origin": "*"
-    }
+      "Access-Control-Allow-Origin": "*",
+    };
     axios
-      .get(
-        `https://api.thelollyapp.com/v1/public/videos/${id}`,
-        { headers: headers }
-      )
-      .then((res) => {        
-        setFlag(1)
+      .get(`https://api.thelollyapp.com/v1/public/videos/${id}`, {
+        headers: headers,
+      })
+      .then((res) => {
+        setFlag(1);
         let arr = {
           firstName: res.data.data.user.firstName,
           dateOfBirth: res.data.data.user.dateOfBirth,
@@ -53,76 +52,88 @@ function Videopage() {
           bio: res.data.data.user.bio,
           cloudflareId: res.data.data.cloudflareId,
           videoWidth: res.data.data.videoWidth,
-          videoHeight: res.data.data.videoHeight
-        }
-        setData(arr)
+          videoHeight: res.data.data.videoHeight,
+        };
+        setData(arr);
         setVideoURL(
           `https://videodelivery.net/${res.data.data.cloudflareId}/downloads/default.mp4`
-        )
-        let birthYear = new Date(res.data.data.user.dateOfBirth * 1000).getFullYear()
-        let nowYear = new Date().getFullYear()
-        setAge(nowYear - birthYear)
+        );
+        let birthYear = new Date(
+          res.data.data.user.dateOfBirth * 1000
+        ).getFullYear();
+        let nowYear = new Date().getFullYear();
+        setAge(nowYear - birthYear);
       })
       .catch((error) => {
-        console.log(error)
-        setFlag(2)
-      })
-  }, [id])
+        console.log(error);
+        setFlag(2);
+      });
+  }, [id]);
+
+  useEffect(() => {
+    console.log("data====", data);
+  }, [data]);
 
   const videoAct = (event) => {
     //desktop
     let videoElement = videoRef.current;
     if (!videoElement.paused) {
-      videoElement.pause()
-      setPlayFlag(true)
+      videoElement.pause();
+      setPlayFlag(true);
     } else {
-      videoElement.play()
-      setPlayFlag(false)
-    } 
+      videoElement.play();
+      setPlayFlag(false);
+    }
 
     //mobile
     let videoElement2 = videoRef2.current;
     if (!videoElement2.paused) {
-      videoElement2.pause()
-      setPlayFlag2(true)
+      videoElement2.pause();
+      setPlayFlag2(true);
     } else {
-      videoElement2.play()
-      setPlayFlag2(false)
-    }    
-  }
-
-  const handleVideoClick = e => {
-    if (!e.target.paused) {
-      e.target.pause()
-      setPlayFlag(true)      
-      setPlayFlag2(true)
+      videoElement2.play();
+      setPlayFlag2(false);
     }
-  }
+  };
+
+  const handleVideoClick = (e) => {
+    if (!e.target.paused) {
+      e.target.pause();
+      setPlayFlag(true);
+      setPlayFlag2(true);
+    }
+  };
 
   return (
     <div className="page">
       {flag === 1 && (
         <>
           <div className="content">
-
-            <div className="video">             
+            <div className="video">
               <video
                 id="vid"
                 width="320"
-                height="240"
+                // height="240"
+                height={data.videoHeight}
                 autoPlay="autoPlay"
                 loop="loop"
-                key={videoURL}   
+                key={videoURL}
                 ref={videoRef}
                 onClick={handleVideoClick}
               >
                 <source src={videoURL} type="video/mp4" />
               </video>
-              {playFlag && <div className="play" onClick={(e) => videoAct(e)}>
-                <img src={Play} alt="play" width="50" />
-              </div>}
+              {playFlag && (
+                <div className="play" onClick={(e) => videoAct(e)}>
+                  <img src={Play} alt="play" width="50" />
+                </div>
+              )}
               <div className="bottommark">
-                <img className="avatar" src={data.profilePicture} alt="avatar" />
+                <img
+                  className="avatar"
+                  src={data.profilePicture}
+                  alt="avatar"
+                />
                 <div className="userinfo">
                   <h4>
                     {data.firstName} | {age} | {data.location}
@@ -149,24 +160,31 @@ function Videopage() {
                 Open app
               </a>
             </div>
-            <div className="video">             
+            <div className="video">
               <video
                 id="vid"
                 width="320"
-                height="240"
+                // height="240"
+                height={data.videoHeight}
                 autoPlay="autoPlay"
                 loop="loop"
-                key={videoURL} 
+                key={videoURL}
                 ref={videoRef2}
                 onClick={handleVideoClick}
               >
                 <source src={videoURL} type="video/mp4" />
-              </video>   
-              {playFlag2 && <div className="play" onClick={(e) => videoAct(e)}>
-                <img src={Play} alt="play" width="50" />
-              </div>}   
+              </video>
+              {playFlag2 && (
+                <div className="play" onClick={(e) => videoAct(e)}>
+                  <img src={Play} alt="play" width="50" />
+                </div>
+              )}
               <div className="bottommark">
-                <img className="avatar" src={data.profilePicture} alt="avatar" />
+                <img
+                  className="avatar"
+                  src={data.profilePicture}
+                  alt="avatar"
+                />
                 <div className="userinfo">
                   <h4>
                     {data.firstName} | {age} | {data.location}
@@ -184,21 +202,21 @@ function Videopage() {
           <div className="pt-5">
             <div className="flex-grow">
               <div className="mx-auto text-white">
-                <div className="align-items-center d-flex flex-row">                 
+                <div className="align-items-center d-flex flex-row">
                   <div className="col-lg-6 error-page-divider text-lg-left pl-lg-4">
                     <h2>SORRY!</h2>
                     <h3 className="font-weight-dark" style={{ color: "black" }}>
                       The page you’re looking for was not found.
                     </h3>
                   </div>
-                </div>            
+                </div>
               </div>
             </div>
           </div>
         </div>
       )}
     </div>
-  )
+  );
 }
 
-export default Videopage
+export default Videopage;
